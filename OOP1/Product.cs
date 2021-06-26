eb2259de8423c7d39549aec238875921d6a652b5
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-06-26 15:58:16 +0300

    CSharp Kurs 4. Gün Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product //Entity
+    {
+        //Bu tip classlarda sadece özellik olur.
+        //snippet - hazır kodlar
+        public int Id { get; set; }
+        public int CategoryId { get; set; } // referans anahtarlar ikinci sıraya yazılır. ( kod okunurluğunu artırmak için)
+        
+        //ürün ismi
+        public string ProductName { get; set; }
+        
+        //ürün fiyatı
+        public double UnitPrice { get; set; }
+        
+        //ürün stok adeti
+        public int UnitsInStock { get; set; }
+
+        //CRUD - Create Read Update Delete - Eklemek Okumak Güncellemek Silmek
+
+    }
+}
